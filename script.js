baa531e055d0d93edecc3e6fa7d6b9b1bb64f621
Xavier Bertrand <150564614+ErreurDeSyntaxe@users.noreply.github.com>
2024-10-09 05:20:49 +0800

    Remove script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-console.log('Hello, World!');
-const body = document.querySelector('body');
-const smallIcon = document.createElement('img');
-
-smallIcon.src = './img/odin-logo.svg';
-
-body.appendChild(smallIcon);
